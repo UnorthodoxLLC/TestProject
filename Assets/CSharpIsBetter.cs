f70fbc056a7ec86eaa3c7a94b52e3d76faf85779
Tai <tjsmith0079@hotmail.com>
2014-08-06 16:26:31 -0600

    made script and a scene

---
+++ Assets/CSharpIsBetter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CSharpIsBetter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
